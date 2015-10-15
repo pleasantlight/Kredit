db7512cc9e2e64468c8382e6a92d75d5745add7a
Noam Etzion-Rosenberg <noam@invi.com>
2015-10-15 13:26:44 -0700

    Initial Commit

---
+++ CoinTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoinTest
+//
+//  Created by Noam Etzion-Rosenberg on 15-Oct-15.
+//  Copyright © 2015 PleasantLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
